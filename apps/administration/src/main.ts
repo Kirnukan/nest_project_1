85a9f377893e535839476e0305dc8976d9039bab
sergej.volobuev <serjant.saygon@gmail.com>
2022-02-10 19:53:46 +0500

    1

--- apps/administration/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AdministrationModule } from './administration.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AdministrationModule);
-  await app.listen(3000);
-}
-bootstrap();
--- apps/market/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { MarketModule } from './market.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(MarketModule);
-  await app.listen(3000);
-}
-bootstrap();
